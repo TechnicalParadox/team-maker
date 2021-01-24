d4bee59e00eaa825d061879750323c0a129b161a
Giamo Lao <giamolao98@gmail.com>
2021-01-24 11:46:09 -0500

    Flowmaster fails tests, this is good

--- __tests__/Flowmaster.test.js
+++ __tests__/Flowmaster.test.js
@@ -8,3 +8,19 @@ test('Flowmaster is created', () =>
   expect(flowmaster.state).toEqual(0);
   expect(flowmaster.available).toBeTruthy();
 });
+
+test('A newly created Flowmaster is able to start', () =>
+{
+  const flowmaster = new Flowmaster();
+  flowmaster.start();
+
+  expect(flowmaster.available).toBeFalsy();
+});
+
+test('An available Flowmaster is able to move on in the flow', () =>
+{
+  const flowmaster = new Flowmaster();
+  flowmaster.next();
+
+  expect(flowmaster.state).toBe(1);
+});
