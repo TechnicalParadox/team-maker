6336cd51043729fe59becf181176b2d1f34f161e
Giamo Lao <giamolao98@gmail.com>
2021-01-23 23:03:27 -0500

    Employee Test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+const sum = require ('./lib/Employee');
+
+test('Employee is created', () =>
+{
+  const employee = new Employee('John', 0, 'john@email.com');
+
+  expect(employee.name).toBe('John');
+  expect(employee.id).toEqual(0);
+  expect(employee.email).toBe('john@email.com');
+
+  const gotName = employee.getName();
+  const gotId = employee.getId();
+  const gotEmail = employee.getEmail();
+  const gotRole = employee.getRole();
+
+  expect(gotName).toBe('John');
+  expect(gotId).toEqual(0);
+  expect(gotEmail).toBe('john@email.com');
+  expect(gotRole).toBe('Employee');
+});
