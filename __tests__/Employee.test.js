b0553f6e60fdad067e18e82f61c867dcbb58d847
Giamo Lao <giamolao98@gmail.com>
2021-01-24 07:53:59 -0500

    Manager and tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,20 +2,42 @@ const Employee = require('../lib/Employee.js');
 
 test('Employee is created', () =>
 {
-  var employee = new Employee('John', 0, 'john@email.com');
+  const employee = new Employee('John', 0, 'john@email.com');
   console.log(employee);
 
   expect(employee.name).toBe('John');
   expect(employee.id).toEqual(0);
   expect(employee.email).toBe('john@email.com');
+});
 
+test('Gets name of employee', () =>
+{
+  const employee = new Employee('John', 0, 'john@email.com');
   const gotName = employee.getName();
-  const gotId = employee.getId();
-  const gotEmail = employee.getEmail();
-  const gotRole = employee.getRole();
 
-  expect(gotName).toBe('John');
-  expect(gotId).toEqual(0);
-  expect(gotEmail).toBe('john@email.com');
-  expect(gotRole).toBe('Employee');
+  expect(gotName).toEqual(expect.stringContaining(employee.name));
+});
+
+test ('Gets ID of employee', () =>
+{
+  const employee = new Employee('John', 0, 'john@email.com');
+  const gotId = employee.getId();
+
+  expect(gotId).toBe(employee.id);
+});
+
+test ('Gets email of employee', () =>
+{
+  const employee = new Employee('John', 0, 'john@email.com');
+
+  const gotEmail = employee.getEmail();
+  expect(gotEmail).toEqual(expect.stringContaining(employee.email));
+});
+
+test ('Gets role of employee', () =>
+{
+  const employee = new Employee('John', 0, 'john@email.com');
+  const gotRole = employee.getRole();
+  
+  expect(gotRole).toEqual('Employee');
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,27 @@
-\
+const Manager = require('../lib/Manager.js');
+
+test('Manager is created', () =>
+{
+  const manager = new Manager("John Doe", 0, "johndoe@email.com", '1-800-123-4567');
+
+  expect(manager.name).toBe("John Doe");
+  expect(manager.id).toEqual(0);
+  expect(manager.email).toBe("johndoe@email.com");
+  expect(manager.officeNumber).toBe('1-800-123-4567');
+});
+
+test('Gets office number of Manager', () =>
+{
+  const manager = new Manager("John Doe", 0, "johndoe@email.com", '1-800-123-4567');
+  const gotON = manager.getOfficeNumber();
+
+  expect(gotON).toBe('1-800-123-4567');
+});
+
+test('Gets role of Manager', () =>
+{
+  const manager = new Manager("John Doe", 0, "johndoe@email.com", '1-800-123-4567');
+  const role = manager.getRole();
+
+  expect(role).toBe('Manager');
+});
